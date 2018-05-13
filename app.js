198b8de8498f8a9572081a41594d1415df9d54f5
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-13 14:55:12 +0300

    Create mongoose connection and query database

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+const Book = require('./db/schemas/book.model');
 
-const PORT_NUMBER = 3000;
+// simple mongoose connection
+const HOSTNAME = 'localhost';
+const PORT = 27017; // default MongoDB port
+const DATABASE_NAME = 'muhdb';
+
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://${HOSTNAME}:${PORT}/${DATABASE_NAME}`).then(
+  () => { console.log('Connected to MongoDB successfylly'); },
+  err => { console.log('error: ' + err);}
+);
+
+app.use(express.static('public'));
 
 // The req and res are the exact same objects that Node provides.
 app.get('/', (req, res) => {
@@ -10,11 +22,21 @@ app.get('/', (req, res) => {
 
 // /books routes should go to their own file. Keep here for now.
 app.get('/books', (req, res) => {
-  res.send('GET /books');
+  Book.find({})
+    .exec((err, books) => {
+      if (err) res.send('Error has occurred');
+      else res.json(books);
+    });
 });
 
 app.get('/books/:id', (req, res) => {
-  res.send('GET /books/id');
+  Book.findOne({
+    _id: req.params.id
+  }).
+  exec((err, book) => {
+    if (err) res.send('Error has occurred');
+    else res.json(book);
+  });
 });
 
 app.post('/books', (req, res) => {
@@ -29,4 +51,9 @@ app.delete('/books/:id', (req, res) => {
   res.send('DELETE /books/id');
 });
 
+app.use((req, res, next) => {
+  res.status(404).send('Sorry can\'t find that!');
+});
+
+const PORT_NUMBER = 3000;
 app.listen(PORT_NUMBER, () => console.log(`Listening on port ${PORT_NUMBER}`));
