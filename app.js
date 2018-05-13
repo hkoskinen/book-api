5c79db3431ced2ef7bfdfd188a890eb076fdaa01
Henri Koskinen <hkoskinen@protonmail.com>
2018-05-13 17:01:18 +0300

    Remove unnecessary code

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
 const books = require('./routes/books');
 
-app.use(express.static('public'));
 app.use('/books', books);
 
 app.get('/', (req, res) => {
-  res.send('Hello to Book API');
+  res.send('Hello from Book API');
 });
 
 const PORT_NUMBER = 3000;
-app.listen(PORT_NUMBER, () => console.log(`Listening on port ${PORT_NUMBER}`));
+app.listen(PORT_NUMBER, () => {
+  console.log(`Listening on port ${PORT_NUMBER}`);
+});
